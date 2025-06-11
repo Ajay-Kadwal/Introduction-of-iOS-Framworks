ad30db69ac18639278c20dda9758c02f1c3fe109
Ajay Kadwal <ajaykadwal.work@gmail.com>
2025-06-11 17:09:01 +0530

    Initial Commit

---
+++ iOSFramworks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSFramworks
+//
+//  Created by AJAY KADWAL on 11/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSFramworks/iOSFramworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSFramworksApp.swift
+//  iOSFramworks
+//
+//  Created by AJAY KADWAL on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSFramworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
